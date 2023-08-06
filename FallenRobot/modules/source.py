18fc69f301fbf02cfb50fac52df53a3b8342e88f
XdRonny <127653254+XdRonny@users.noreply.github.com>
2023-08-07 01:19:54 +0530

    Update source.py
    Signed-off-by: XdRonny <127653254+XdRonny@users.noreply.github.com>

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -29,7 +29,7 @@ async def repo(_, message: Message):
                     InlineKeyboardButton("● ᴅᴇᴠᴇʟᴏᴘᴇʀ ●", user_id=OWNER_ID),
                     InlineKeyboardButton(
                         "● sᴏᴜʀᴄᴇ ●",
-                        url="https://t.me/Dairy_dash",
+                        url="https://t.me/Life1GoesOn",
                     ),
                 ]
             ]
